8ef99815f9d86a915b330bf8046f84e4946b2a18
Nicko J. Ruddock <exterminater999@gmail.com>
2018-01-08 20:16:52 +0000

    auto-formatted file for neatness

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,33 @@
-	// Modules
+// Modules
 var gulp = require("gulp"),
 	newer = require("gulp-newer"),
 	imagemin = require("gulp-imagemin"),
-	htmlmin = require('gulp-htmlmin');
+	htmlmin = require('gulp-htmlmin'),
 
 	// folders
 	folder = {
 		src: "src",
 		build: "dist"
-	}
-;
+	};
 
 // Image processing
-gulp.task("images", function(){
+gulp.task("images", function () {
 	var out = `${folder.build}/imgs/`;
 	return gulp.src(`${folder.src}/assets/imgs/**/*`)
 		.pipe(newer(out))
-		.pipe(imagemin({optimizationLevel: 5}))
+		.pipe(imagemin({
+			optimizationLevel: 5
+		}))
 		.pipe(gulp.dest(out));
 });
 
 // PHP processing
-gulp.task("php", ["images"], function() {
+gulp.task("php", ["images"], function () {
 	var out = folder.build;
 	return gulp.src(`${folder.src}/*.php`)
 		.pipe(newer(out))
-		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(htmlmin({
+			collapseWhitespace: true
+		}))
 		.pipe(gulp.dest(out));
 });
