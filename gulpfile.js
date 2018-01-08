9b698b5d3d6f7392e03ac1abcf5c71efca7e8366
Nicko J. Ruddock <exterminater999@gmail.com>
2018-01-08 19:49:14 +0000

    Added PHP task

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,29 @@
 var gulp = require("gulp"),
 	newer = require("gulp-newer"),
 	imagemin = require("gulp-imagemin"),
-	
-	
+	htmlmin = require('gulp-htmlmin');
+
 	// folders
 	folder = {
-		src: "src/",
-		build: "build/"
+		src: "src",
+		build: "dist"
 	}
 ;
 
 // Image processing
 gulp.task("images", function(){
-	var out = folder.build + "imgs/";
-	return gulp.src(folder.src + "assets/imgs/**/*")
+	var out = `${folder.build}/imgs/`;
+	return gulp.src(`${folder.src}/assets/imgs/**/*`)
 		.pipe(newer(out))
-		.pipe(imagemin({ optimizationLevel: 5 }))
+		.pipe(imagemin({optimizationLevel: 5}))
 		.pipe(gulp.dest(out));
 });
 
 // PHP processing
+gulp.task("php", ["images"], function() {
+	var out = folder.build;
+	return gulp.src(`${folder.src}/*.php`)
+		.pipe(newer(out))
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest(out));
+});
