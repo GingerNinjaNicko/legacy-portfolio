a4ce48701c81be316c079face3151893a0c63d49
Nicko J. Ruddock <exterminater999@gmail.com>
2018-01-17 19:24:45 +0000

    Added aria-labels to links w/ no text

--- src/index.php
+++ src/index.php
@@ -213,20 +213,20 @@
                                 I always try to respond to messages within 48 hours so you know your message is in safe hands!
                             </p>
                             <p>
-                                <a href="https://codepen.io/GingerNinjaNicko/" target="_blank">
+                                <a href="https://codepen.io/GingerNinjaNicko/" target="_blank" aria-label="Visit my Codepen page">
                                     <i title="Visit my Codepen page" class="fa fa-codepen" aria-hidden="true"></i>
                                 </a>
-                                <a href="https://github.com/GingerNinjaNicko" target="_blank">
+                                <a href="https://github.com/GingerNinjaNicko" target="_blank" aria-label="Visit my Github page">
                                     <i title="Visit my Github page" class="fa fa-github" aria-hidden="true"></i>
                                 </a>
-                                <a href="https://medium.com/@GingerNinjaNick" target="_blank">
+                                <a href="https://medium.com/@GingerNinjaNick" target="_blank" aria-label="Visit my Medium page">
                                     <i title="Visit my Medium page" class="fa fa-medium" aria-hidden="true"></i>
                                 </a>
-                                <a href="https://www.facebook.com/GingerNinjaNick" target="_blank">
+                                <a href="https://www.facebook.com/GingerNinjaNick" target="_blank" aria-label="Visit my Facebook page">
                                     <i title="Visit my Facebook page" class="fa fa-facebook-official" aria-hidden="true"></i>
                                 </a>
                                 <!--
-                                <a href="https://www.linkedin.com/in/gingerninjanicko/">
+                                <a href="https://www.linkedin.com/in/gingerninjanicko/" target="_blank" aria-label="Visit my Linkedin page">
                                     <i title="Visit my Linkedin page" class="fa fa-linkedin-square" aria-hidden="true"></i>
                                 </a>
                                 -->
