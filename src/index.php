6a9355cfac82954a95587aea1529fd9437e2a261
Nicko J. Ruddock <exterminater999@gmail.com>
2018-01-17 17:48:18 +0000

    scripts now loaded async

--- src/index.php
+++ src/index.php
@@ -23,7 +23,7 @@
     <link rel="stylesheet" href="./assets/css/main.css">
     
     <!-- Font awesome -->
-    <script src="https://use.fontawesome.com/ab45073717.js"></script>
+    <script async src="https://use.fontawesome.com/ab45073717.js"></script>
     
     <!-- If javascript not enabled, form submits to this page so include php form logic-->
     <noscript>
@@ -281,16 +281,16 @@
     </footer>
 
     <!-- Javascript files -->
-    <script 
+	<script async
 		src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous">
 	</script>
-    <script
+	<script async
 		src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous">
 	</script>
-    <script
+	<script async
 		src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous">
 	</script>
-    <script 
+	<script async
 		type="text/javascript" 
 		src="./assets/js/main.min.js">
 	</script>
